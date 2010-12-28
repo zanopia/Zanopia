d117054184241b8f908683a530127d88f8df851f
poz <guillaume@bk.ru>
2010-12-28 15:41:26 +0100

    benchmark results for imaptest/dovecot and glusterfs and an average hardware

--- dovecot_benchmark/setup/glusterfs/bench-glusterfs.sh
+++ dovecot_benchmark/setup/glusterfs/bench-glusterfs.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-IMAPTEST=/root/benches-imaptest/imaptest-20100922/src/imaptest
-MBOX=/root/benches-imaptest/dovecot-crlf
+IMAPTEST=/root/imaptest-20100922/src/imaptest
+MBOX=/root/dovecot-crlf
 USERFILE=/etc/dovecot/userlist
-OUTDIR=/root/benches-imaptest/benches_imaptest/glusterfs_fuse
-SFUSED=/root/sfused
-CREATE_DIR_SCRIPT=/root/benches-imaptest/c.sh
+OUTDIR=/root/benches-imaptest/glusterfs-fuse
+CREATE_DIR_SCRIPT=/root/c.sh
 
 
 for c in $(seq -w 10 10 500); do
@@ -14,24 +13,23 @@ for c in $(seq -w 10 10 500); do
 	> /var/log/dovecot.log
 	> /var/log/dovecot-info.log
 
-	echo "[*] Umounting /mnt/glusterfs"
-	sync
-	fusermount -u /mnt/glusterfs
+	echo "[*] Umounting /mnt/dovecot"
+	fusermount -u /mnt/dovecot
 	sleep 2
 
 	echo "[*] Mounting GlusterFS..."
-	mount -t glusterfs /etc/glusterfs/glusterfs.vol /mnt/glusterfs
+	mount -t glusterfs /etc/glusterfs/glusterfs.vol /mnt/dovecot
 	sleep 2
 
 	echo "[*] Removing index directories: rm -rf /mnt/localstorage/*"
 	rm -rf /mnt/localstorage/*
 	sleep 2
 
-	echo "[*] Creating all the users' directories (/mnt/glusterfs/test* and /mnt/localstorage/test*)"
-	(for i in $(seq -w 500); do
- 		echo test00$i
-  	done) | xargs -n1 -P 50 $CREATE_DIR_SCRIPT
-	sleep 2
+#	echo "[*] Creating all the users' directories (/mnt/dovecot/test* and /mnt/localstorage/test*)"
+#	(for i in $(seq -w 500); do
+#	echo test$i
+#	done) | xargs -n1 -P 50 $CREATE_DIR_SCRIPT
+#	sleep 2
 
 	echo "[*] Chown-ing doveusers:doveusers /mnt/localstorage"
 	chmod 777 /mnt/localstorage
